914ba9ddd7d774cbe325fad8177b2624534996b6
Couvbat <mrcouvbat@gmail.com>
2023-12-04 17:28:57 +0100

    did some changes

--- pages/components/TimerTab.jsx
+++ pages/components/TimerTab.jsx
@@ -1,27 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Stopwatch } from "react-native-stopwatch-timer";
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useStopwatch } from 'react-timer-hook';
 
-export function TimerTab({ startTime, isRunning }) {
+const padTime = (time) => String(time).padStart(2, '0');
+
+export function TimerTab({ elapsedTime, isRunning }) {
+  const {
+    seconds,
+    minutes,
+    hours,
+    start,
+    reset,
+  } = useStopwatch({ autoStart: false });
+
+  useEffect(() => {
+    if (isRunning) {
+      // Only reset if elapsedTime changes
+      // Note: Add a check to ensure elapsedTime is not null
+      if (elapsedTime) {
+        reset(elapsedTime ? elapsedTime / 1000 : 0, elapsedTime !== undefined);
+        start();
+      }
+    } else {
+      reset();
+    }
+    // Remove lastTimestamp from the dependency array if not used
+  }, [isRunning, elapsedTime, reset, start]);
+
   return (
-    <>
-      <View style={styles.wrapper}>
-        {!isRunning && (<Text style={styles.loading}>Chargement...</Text>)}
-
-        {isRunning && (
-          <>
-            <Text style={styles.text}>Dernier Timestamp créé il y a : </Text>
-            <Stopwatch
-              options={styles.stopwatch}
-              laps
-              start={isRunning}
-              startTime={startTime}
-            />
-          </>
-        )}
-      </View>
-    </>
+    <View style={styles.wrapper}>
+      {isRunning ? (
+        <Text style={styles.text}>
+          Dernier Timestamp créé il y a : {padTime(hours)}:{padTime(minutes)}:{padTime(seconds)}
+        </Text>
+      ) : (
+        <Text style={styles.loading}>Chargement...</Text>
+      )}
+    </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   wrapper: {
